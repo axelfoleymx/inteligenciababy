8ca2a3e9473b8a6fbb03b662e9a717acd69f825a
¨axelfoleymx¨ <a01164173@gmail.com>
2016-07-14 21:02:26 -0500

    solo una vez responde

--- JS/main.js
+++ JS/main.js
@@ -1,27 +1,36 @@
 
-
-
-// escuchar
-
-arrayRespuesta = ["¡Que onda!", "Buenas tardes", "Buenos días", "Buenas noches", "¡Qué tal!", "Hello, bitch!", "Baby one more time...",
+var respuestas = ["que pedo", "hola", "como estas?", "vete de aqui", "fuera de aqui", "no me hables",
+"llegale", "muy bien", "wow", "cool", "apestas", "largo", "callate", "no me importa", "callese","¡Que onda!", "Buenas tardes", "Buenos días", "Buenas noches", "¡Qué tal!", "Hello, bitch!", "Baby one more time...",
 "Elemental mi querido Watson", "May the force be with you", "Que el poder te protega", "¡Oye como va!", "The winter is coming", "Good save the Queen",
 "I want to ride my bicicle", "The King of the North", "Valar Morghulis", "Valar Dohaeris", "¿Dónde quedo la cadenita?", "Why so sad?", "¿Me estás habland a mi?",
 ]
+var  aleatorio = Math.round(Math.random(aleatorio)*respuestas.length);
 
+console.log(respuestas[aleatorio]);
+
+function responder(){
+	$(".pregunta").html(respuestas[aleatorio]);
+
+}
+for (i = 0; i <=10; i++){ 
 $(document).keypress(function(e){
 
 	if(e.which == 13){
-	console.log("enter");
+	// console.log("enter");
 
-		var input=$("#input").val();
-		console.log(input);
-		$("input").val("");
-		$("div.pregunta").replaceWith("<h1>Hey, bitch</h1>");
+    var input=$("#input").val();
+	// 	console.log(input);
+	// 	$("input").val("");
+
+		responder();
 
 
 	}
 
 }); 
+}
+
+
 
 
 // function clearInputs(data){
